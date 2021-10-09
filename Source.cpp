2eac0faf5450df17794728399e9c0476b2d3a053
IIIxIIIx <91841270+IIIxIIIx@users.noreply.github.com>
2021-10-09 19:45:33 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>Ракович Алексей П43021
+using namespace std;
+int main() {
+	/*//Boolean8◦. Даны два целых числа : A, B.Проверить истинность высказывания :«Каждое из чисел A и B нечетное».
+	int a, b;
+	bool res;
+	cin >> a >> b;
+	res=(a % 2 == 1 && b % 2 == 1);
+	cout << boolalpha << res << endl;
+	return 0;
+	//Boolean9◦. Даны два целых числа : A, B.Проверить истинность высказывания :«Хотя бы одно из чисел A и B нечетное».
+	int a, b;
+	bool res;
+	cin >> a, b;
+	res= (a % 2 == 1 || b % 2 == 1);
+	cout << boolalpha << res << endl;
+	return 0;
+	//Boolean11◦. Даны два целых числа : A, B.Проверить истинность высказывания : «Числа A и B имеют одинаковую четность».
+	//незнаю как решить
+	return 0;
+	//Boolean12◦. Даны три целых числа : A, B, C.Проверить истинность высказывания : «Каждое из чисел A, B, C положительное».
+	int a, b, c;
+	bool res;
+	cin >> a >> b >> c;
+	res = a > 0 && b > 0 && c > 0;
+	cout << boolalpha << res << endl;
+	return 0;
+	//Boolean14◦. Даны три целых числа : A, B, C.Проверить истинность высказывания : «Ровно одно из чисел A, B, C положительное».
+	int A, B, C;
+	bool res;
+	cin >> A >> B >> C;
+	res = (A > 0 && B < 0 && C < 0) ||
+		(A < 0 && B>0 && C < 0) ||
+		(A < 0 && B < 0 && C>0);
+	cout << boolalpha << res <<endl;
+	return 0;*/
+
+}
